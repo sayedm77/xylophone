73d057cb569d74fcbdf4fc287b8a0dae0d90e0a5
Elsayed Mansour <118473747+sayedm77@users.noreply.github.com>
2024-03-22 00:29:01 +0200

    Update ViewController.swift

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 import AVFoundation
